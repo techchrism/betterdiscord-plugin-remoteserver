ff55a345f6889dc5bde33e0a75ed2757ea485a66
techchrism <chris@techchrism.me>
2022-06-01 16:23:45 -0700

    update config loading

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
 import {promises as fs} from 'fs';
+import path from 'path';
 import {nanoid} from 'nanoid';
 
 module.exports = class RemoteServer {
     async start() {
         let config = {
-            ip: '127.0.0.1',
+            host: '127.0.0.1',
             port: 26745,
             password: nanoid()
         };
         const configFileName = 'RemoteServer.config.json';
+        const configPath = path.join(BdApi.Plugins.folder, configFileName);
         try {
-            config = JSON.parse(await fs.readFile(configFileName, 'utf-8'));
+            config = JSON.parse(await fs.readFile(configPath, 'utf-8'));
         } catch(e) {
             console.warn('Failed reading RemoteServer config, writing default config...');
-            await fs.writeFile(configFileName, JSON.stringify(config), 'utf-8');
+            await fs.writeFile(configPath, JSON.stringify(config, null, 4), 'utf-8');
         }
 
-
+        console.log(`Loaded from ${configPath}`);
     }
     stop() {
 
